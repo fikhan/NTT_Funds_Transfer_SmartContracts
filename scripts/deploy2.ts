6c0cc36dad150e014b28fe3dbb8b6fce041b58e8
fkhan <fkhan@ejada.com>
2022-05-06 21:32:07 +0300

    Deploying Badge Contract and Reciever Contract

--- scripts/deploy2.ts
+++ scripts/deploy2.ts
@@ -11,6 +11,7 @@ import type { Artifact } from "hardhat/types";
  import type { ERC1238ReceiverMock } from "./src/types/ERC1238RecieverMock";
 
 async function main() {
+
   // Hardhat always runs the compile task when running scripts with its command
   // line interface.
   //
@@ -20,12 +21,17 @@ async function main() {
 
   // We get the contract to deploy
   const [owner, addr1, addr2] = await ethers.getSigners();
-  // const Badge = await ethers.getContractFactory("Badge");
-  // const badge = await Badge.deploy(owner.address,"");
 
-  // await badge.deployed();
+  /************************Deploying the Badge Contract************************/
 
-  // console.log("Badge deployed to:", badge.address);
+  const Badge = await ethers.getContractFactory("Badge");
+  const badge = await Badge.deploy(owner.address,"");
+
+  await badge.deployed();
+
+  console.log("Badge deployed to:", badge.address);
+
+  /***********************Deploying Reciever Smart Contract *******************/
    let smartContractRecipient1: ERC1238ReceiverMock;
   let tokenRecipient: SignerWithAddress;
   const signers: SignerWithAddress[] = await ethers.getSigners();
@@ -37,42 +43,6 @@ async function main() {
 
   console.log("Recipient is deployed at address:", smartContractRecipient1.address )
 
-  // const erc1238reciever = await ethers.getContractFactory("ERC1238ReceiverMock");
-//  let add1 = ethers.utils.getAddress("0x5BE6E77b69BD3d9418712E21B26b920CA86478Cc")
-//  const erc1238 = await erc1238reciever.deploy(add1);
-
-  // await erc1238.deployed();
-
-  // console.log("ERC1238 receiver 1 deployed to:", erc1238.address);
-
-  // const erc1238reciever1 = await ethers.getContractFactory("ERC1238ReceiverMock");
-  // const erc12381 = await erc1238reciever1.deploy("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266");
-
-  // await erc12381.deployed();
-
-  // console.log("ERC1238 receiver 2 deployed to:", erc12381.address);
-
-  // const erc1238reciever2 = await ethers.getContractFactory("ERC1238ReceiverMock");
-  // const erc12382 = await erc1238reciever2.deploy("0x70997970C51812dc3A010C7d01b50e0d17dc79C8");
-
-  // await erc1238.deployed();
-
-  // console.log("ERC1238 receiver 3 deployed to:", erc12382.address);
-
-
-  // const erc1238reciever3 = await ethers.getContractFactory("ERC1238ReceiverMock");
-  // const erc12383 = await erc1238reciever3.deploy("0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC");
-
-  // await erc12383.deployed();
-
-  // console.log("ERC1238 receiver 4 deployed to:", erc12383.address);
-
-  // const erc1238reciever4 = await ethers.getContractFactory("ERC1238ReceiverMock");
-  // const erc12384 = await erc1238reciever4.deploy("0x236264b88b4221817FB853B03a3369E275B32344");
-
-  // await erc12384.deployed();
-
-  // console.log("ERC1238 receiver 5 deployed to:", erc12384.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
